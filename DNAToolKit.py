34b9f2b662a878eca527a00788c8dbbda336ff68
abdelrahman992-cpu <abdelrahmandarrage2020@gmail.com>
2022-07-03 03:43:37 +0200

    add

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -17,3 +17,13 @@ def transcription(seq):
     return seq.replace("T","U")
 def reverseComplement(seq):
     return ''.join([DNAreverseComplement[nuc] for nuc in seq])[::-1]
+def GCcontent(seq):
+    return round((seq.count('C')+seq.count('G')) / len(seq) * 100)
+def gc_content_subsec(seq, k=20):
+        """GC Content in a DNA/RNA sub-sequence length k. k=20 by default"""
+        res = []
+        for i in range(0, len(seq) - k + 1, k):
+            subseq = seq[i:i + k]
+            res.append(
+                round((subseq.count('C') + subseq.count('G')) / len(subseq) * 100))
+        return res
--- main1.py
+++ main1.py
@@ -1,17 +1,24 @@
+import colorama
 from DNAToolKit import *
-from colorama import init
+from colorama import Fore , Back,Style
 from utilities import colored
 import random
-
-rndDNAStr=''.join([random.choice(Nucleotides) for nuc in range(20)])
+colorama.init()
+rndDNAStr=''.join([random.choice(Nucleotides) for nuc in range(50)])
 DNAStr=validateSeq(rndDNAStr)
 print("\n seqeunce\n"+colored(DNAStr)+"\n")
 print("[1] seqeuncelength")
 print(len(DNAStr))
 print("\n[2] Nuleotide Frequency")
-print(countNucFrequency(DNAStr))
+print(colored(countNucFrequency(DNAStr)))
 print("\n[3] DNA/RNA Transcription")
-print(transcription(DNAStr))
-print("5`-"+DNAStr+"-3`")
+print(colored(transcription(DNAStr)))
+print("[4] DNA String + complement+ Reverse complement")
+print("5`-"+colored(DNAStr)+"-3`")
 print("   "+''.join(['|'for c in range(len(DNAStr))]))
-print("3`-"+reverseComplement(DNAStr)+"-5`")
+print("3`-"+colored(reverseComplement(DNAStr)[::-1])+"-5` Complement")
+print("5`-"+colored(reverseComplement(DNAStr))+"-3` Rev Complement")
+print("[5]GC Content")
+print(GCcontent(DNAStr))
+print("[6] GC content in subsection k=5")
+print(gc_content_subsec(DNAStr,k=5))
